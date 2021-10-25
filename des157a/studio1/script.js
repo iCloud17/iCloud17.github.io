08b462c937a160a983eda6863adba3309cf7012d
Baadal Bhojak <baadal.bhojak@gmail.com>
2021-10-24 22:20:58 -0700

    finished madlibs!

--- des157a/studio1/script.js
+++ des157a/studio1/script.js
@@ -4,7 +4,7 @@
 
     //------------------Resize backpage to be same as main form page------------------
     let frame = document.getElementById('rotatedFrame');
-    let formPage = document.getElementById('mainPage');
+    let mainPage = document.getElementById('mainPage');
 
     function resizeFrame(toSet, toCopy) {
         toSet.style.height = toCopy.offsetHeight + 'px';
@@ -12,14 +12,14 @@
     }
 
     window.addEventListener('resize', function(event) {
-        resizeFrame(frame, formPage);
+        resizeFrame(frame, mainPage);
     });
 
-    resizeFrame(frame, formPage);
+    resizeFrame(frame, mainPage);
 
     //-------------------------------Random Spawn Ghost-------------------------------
     const ghost = document.getElementById('ghost');
-    const spawns = [[15, 10, 50, 0], [15, 7, 75, 30], [20, 58, 7, -15], [10, 55, 80.5, 30], [30, 68, 77, -10]];
+    const spawns = [[15, 2, 41, 0], [15, 2, 80, 30], [20, 55, 8, -15], [10, 60, 83, 30], [25, 73, 83, -10]];
     const transitions = ['ease', 'ease-in', 'ease-in-out', 'ease-out'];
 
     function getRandomInt(lo, hi) {
@@ -51,5 +51,49 @@
         }, duration);
     }
 
-    // var timer =  setInterval(spawnGhost, 15000);
+    var timer =  setInterval(spawnGhost, 15000);
+
+    //------------------------Function To Hide and Show Element------------------------
+    function toDisplay(element, show) {
+        if(show) {
+            element.style.visibility = 'visible';
+            element.style.opacity = '1';
+        } else {
+            element.style.visibility = 'hidden';
+            element.style.opacity = '0';
+        }
+    }
+
+    //---------------------------------Submitting Form---------------------------------
+
+    function assignVal(id1, id2) {
+        for(let id of id1) {
+            document.getElementById(id).innerText = document.getElementById(id2).value;
+        }
+        document.getElementById(id2).value = '';
+    }
+
+    const formPage = document.getElementById('formPage');
+    const form = document.getElementById('myForm');
+    const story = document.getElementById('story');
+    const again = document.querySelector('#story button');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        assignVal(['fr1', 'fr11', 'fr12'], 'relation1');
+        assignVal(['vb1'], 'verb');
+        assignVal(['loc'], 'room1');
+        assignVal(['fr2'], 'relation2');
+        assignVal(['ad1'], 'adj1');
+        assignVal(['feeling'], 'feel1');
+
+        toDisplay(formPage, false);
+        toDisplay(story, true);
+    });
+
+    //-----------------------------------Play Again-----------------------------------
+    again.addEventListener('click', function(event) {
+        toDisplay(formPage, true);
+        toDisplay(story, false);
+    });
 }());
